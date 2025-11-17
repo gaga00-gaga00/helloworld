1be25f949834d5f99eec8545b02924c938bc1985
gaga00-gaga00 <gagaooyes401@yonsei.ac.kr>
2025-11-17 15:56:20 +0900

    파일추가1

---
+++ gimal_main_1113.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <math.h>
+#include "Pj.h"
+
+void main() 
+{
+	linkedlist* L;
+	listwork* p;
+
+	int first;
+	int second;
+
+	printf("현재 과제 목록\n");
+	//리스트 쫙 보여주기
+
+	printf("(1) 마감까지 남은 시간 보기\n");
+	printf("(2) 알림 시간대 설정\n");
+	printf("(3) 과제 추가\n");
+	printf("(4) 과제 삭제\n");
+	printf("(5) 과제 수정\n");
+	printf("(6) 과제 검색\n");
+
+	scanf("%d", &first);
+
+	switch (first)//어디서부터 꼬인거야~
+	{
+	case 1:
+	{
+		break;
+	}
+	case 2:
+	{
+		break;
+	}
+	case 3:
+	{
+		break;
+	}
+	case 4:
+	{
+		break;
+	}
+	case 5:
+	{
+		break;
+	}
+	case 6:
+	{
+		break;
+	}
+	}
+}
